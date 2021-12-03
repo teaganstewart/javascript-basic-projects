9e67ad3ee451dfff2da492ca48c63a9396c89507
Teagan Stewart <65583615+teaganstewart@users.noreply.github.com>
2021-12-04 11:51:23 +1300

    Color Flipper Complete
    Hex and Simple version work.
    - easy enough

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,25 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".colour");
+
+btn.addEventListener("click", function() {
+    const randomHex = getRandomHex();
+    document.body.style.backgroundColor = randomHex;
+    color.textContent = randomHex;
+});
+
+function getRandomHex() {
+
+    let hexValue = "#";
+    for(let i = 0; i < 6; i++) {
+        hexValue += hex[getRandomValue()];
+    }
+   
+    return hexValue;
+}
+
+function getRandomValue() {
+    return Math.floor(Math.random() * hex.length);
+}
